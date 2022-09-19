592f20b41b249852c1ddfba2b768d3de8b787e7b
Karabas890 <113842344+Karabas890@users.noreply.github.com>
2022-09-19 19:41:56 +0300

    Update Lab1.java

--- Lab1.java
+++ Lab1.java
@@ -1,15 +1,15 @@
 public class Lab1 {
-    private static short[] GetA() {
+    private static short[] getArrayA() {
         short[] a=new short[11];
         int index=0;
         for(int i=22;i>=2;i=i-2){
             a[index]=(short)i;
             index+=1;
-
+        //    System.out.println(i);
         }
         return a;
     }
-    private static double[] GetX() {
+    private static double[] getArrayX() {
         double[] x=new double[12];
         for(int i=0;i<12;i++){
             x[i]=  ((Math.random() * (10 - (-10))) + (-10));
@@ -17,12 +17,12 @@ public class Lab1 {
         }
         return x;
     }
-    private static void FormatPrint(double array[][]){
+    private static void formatPrint(double array[][]){
         for(int i = 0; i<11; i++){
             for(int j = 0; j<12; j++){
-                System.out.printf("%10.4f", array[i][j]);
+               printf("%10.4f", array[i][j]);
             }
-            System.out.println();
+            print("\n");
         }
     }
 
@@ -30,21 +30,38 @@ public class Lab1 {
 
 
         double[][] t=new double[11][12];
-        short a[]=GetA();
-        double x[]=GetX();
+        short a[]=getArrayA();
+        double x[]=getArrayX();
         for(int i=0;i<11;i++){
-            for(int j=0;j<12;j++){
-                if(a[i]==12){
-                    t[i][j]=Math.pow((Math.pow(2*Math.tan(x[j]),3)-1/3.0),(Math.sin(Math.pow(x[j],x[j]/4))));
+            for(int j=0;j<12;j++) {
+                switch (a[i]){
 
-                }else if(a[i]==6 || a[i]==8|| a[i]==10||  a[i]==14 || a[i]==16){
-                    t[i][j]=Math.atan(Math.pow(Math.E,Math.cbrt((-1)*Math.pow(Math.tan(x[j]),2))));
+                    case  12:
+                        t[i][j] = Math.pow((Math.pow(2 * Math.tan(x[j]), 3) - 1 / 3.0), (Math.sin(Math.pow(x[j], x[j] / 4))));
+                        break;
+                    case 6,8,10,14,16:
+                        t[i][j] = Math.atan(Math.pow(Math.E,Math.cbrt((-1)*Math.pow(Math.tan(x[j]),2))));
+                        break;
 
-                }else{
-                    t[i][j]=Math.cbrt(Math.atan(1/(Math.pow(Math.E,Math.pow(Math.tan(Math.pow(2/(Math.asin(x[j]/2*Math.E+1)-1/4.0),Math.log(Math.abs(x[j])))),2)))));
+
+                    default:
+                        t[i][j] = Math.cbrt(Math.atan(1 / (Math.pow(Math.E, Math.pow(Math.tan(Math.pow(2 / (Math.asin(x[j] / 2 * Math.E + 1) - 1 / 4.0), Math.log(Math.abs(x[j])))), 2)))));
+                        break;
                 }
             }
         }
-        FormatPrint(t);
+        formatPrint(t);
     }
+
+
+    public static void print(String m){
+        System.out.print(m);
+    }
+    public static void printf(String p,double x){
+        System.out.printf(p,x);
+    }
+
+
+
 }
+
